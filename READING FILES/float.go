201aaca19bbf4594017850894c12d495a5b5fe23
Даниил Лоллин <gbeorg24@gmail.com>
2025-01-24 00:47:06 +0300

    Create float.go

---
+++ READING FILES/float.go
@@ -0,0 +1,34 @@
+package datafile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func GetFloats(filename string) ([3]float64, error) {
+
+	var numbers [3]float64
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return numbers, err
+	}
+	i := 0
+	scaner := bufio.NewScanner(file)
+	for scaner.Scan() {
+		numbers[i], err = strconv.ParseFloat(scaner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		i++
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scaner.Err() != nil {
+		return numbers, scaner.Err()
+	}
+	return numbers, nil
+}
