d391772cb9d02e4397b4371a79162d23df2c4367
Даниил Лоллин <gbeorg24@gmail.com>
2025-01-24 17:18:43 +0300

    Update Reading-Directory.go

--- Reading-Directory.go
+++ Reading-Directory.go
@@ -3,33 +3,25 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 )
 
-func ScanDirectory(path string) error {
+func ScanDirectory(path string) {
 	fmt.Println(path)
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
 		if file.IsDir() {
-			err := ScanDirectory(filePath)
-			if err != nil {
-				log.Fatal(err)
-			}
+			ScanDirectory(filePath)
 		} else {
 			fmt.Println(filePath)
 		}
 	}
-	return nil
 }
 func main() {
-	err := ScanDirectory("D:")
-	if err != nil {
-		log.Fatal(err)
-	}
+	ScanDirectory("D:")
 
 }
